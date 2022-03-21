499e38ea9325e17129bd1267cb60a92fad8992e7
habuild <36288425+habuild@users.noreply.github.com>
2022-03-21 12:10:01 +1100

    Update taillog.sh

--- sbfspot/rootfs/etc/sbfspot/taillog.sh
+++ sbfspot/rootfs/etc/sbfspot/taillog.sh
@@ -5,6 +5,8 @@ cd /
 # create placeholder to avoid delay on real log
 #echo [$(date)] INFO: SBFspotUpload Logging started > /data/sbfspot/log/fake.log
 
+sleep 20
+
 # set tail to log out to HA log
 echo 'logging commenced'
 tail -n1 /data/sbfspot/log/* >> /proc/1/fd/1
